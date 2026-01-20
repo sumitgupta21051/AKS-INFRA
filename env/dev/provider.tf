ba896f182018853a22661efd7f365ee874d1dc95
unknown <sumitgupta21051@gmail.com>
2026-01-21 02:39:09 +0530

    l5

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -6,15 +6,16 @@ terraform {
     }
   }
   backend "azurerm" {
-    use_oidc              = true
-    tenant_id= "66c291b4-1789-4bcb-9817-6b0339a238bc"
-    client_id= "24173b11-ec1b-4072-813a-0aa8e3e81acf"
-    resource_group_name  = "sumit"      
-    storage_account_name = "sumit1"      
-    container_name       = "sumit1"      
-    key                  = "dev.tfstate" 
+    use_oidc             = true
+    use_azuread_auth     = true
+    tenant_id            = "66c291b4-1789-4bcb-9817-6b0339a238bc"
+    client_id            = "24173b11-ec1b-4072-813a-0aa8e3e81acf"
+    resource_group_name  = "sumit"
+    storage_account_name = "sumit1"
+    container_name       = "sumit1"
+    key                  = "dev.tfstate"
   }
-  
+
 }
 
 provider "azurerm" {
