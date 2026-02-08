e8136707ed25b83e7de4fab3e7b8c2b6165fe1b7
unknown <sumitgupta21051@gmail.com>
2026-02-08 07:46:37 +0530

    jon1

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -11,8 +11,8 @@ terraform {
     tenant_id            = "66c291b4-1789-4bcb-9817-6b0339a238bc"
     client_id            = "24173b11-ec1b-4072-813a-0aa8e3e81acf"
     resource_group_name  = "sumit"
-    storage_account_name = "sumit1"
-    container_name       = "sumit1"
+    storage_account_name = "sumitbackend"
+    container_name       = "sumitbackend"
     key                  = "dev.tfstate"
   }
 
@@ -20,5 +20,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "cef3b1fa-2c9d-4212-843c-0107fc35ca77"
+  subscription_id = "580eec40-34b7-4c9b-ba06-bd14c2347042"
 }
