2bb5116e9ffb16e5dc41eee729f0a6b916372b56
unknown <sumitgupta21051@gmail.com>
2026-06-06 15:49:24 +0530

    la

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,30 +1,30 @@
-module "rg" {
-  source = "../../modules/azurerm_resource_group"
-  rgs    = var.rgs
-}
+# module "rg" {
+#   source = "../../modules/azurerm_resource_group"
+#   rgs    = var.rgs
+# }
 
-module "acr" {
-    depends_on = [ module.rg ]
-  source = "../../modules/azurerm_container_registry"
-  acr    = var.acr
-}
+# module "acr" {
+#     depends_on = [ module.rg ]
+#   source = "../../modules/azurerm_container_registry"
+#   acr    = var.acr
+# }
 
 
-module "sql_server" {
-    depends_on = [ module.rg ]
-    source = "../../modules/azurerm_sql_server"
-    sqlserver= var.sqlserver
-}
+# module "sql_server" {
+#     depends_on = [ module.rg ]
+#     source = "../../modules/azurerm_sql_server"
+#     sqlserver= var.sqlserver
+# }
 
-module "sql_database"{
-    depends_on = [ module.sql_server ]
-    source= "../../modules/azurerm_sql_database"
-    sql_datbase= var.sql_datbase
-}
+# module "sql_database"{
+#     depends_on = [ module.sql_server ]
+#     source= "../../modules/azurerm_sql_database"
+#     sql_datbase= var.sql_datbase
+# }
 
-module "aks" {
-    depends_on = [ module.rg ,module.acr]
-    source = "../../modules/azurerm_kubernetes_cluster"
-    aks=var.aks
-}
+# module "aks" {
+#     depends_on = [ module.rg ,module.acr]
+#     source = "../../modules/azurerm_kubernetes_cluster"
+#     aks=var.aks
+# }
 
